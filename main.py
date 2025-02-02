761095a57bfc9775fd88e9e5d1dd52c6e80603d0
Xenotime <161914158+xnotime@users.noreply.github.com>
2025-02-01 23:31:31 -0600

    OKEY DOKEY

--- main.py
+++ main.py
@@ -7,32 +7,40 @@ def readmol(name: str) -> GraphMolecule:
         label= True,
     )
 
-
-
 class Top(Scene):
     def construct(self):
+        left0 = MathTex('< 140\\,{}^{\\circ} \\text{C}').to_corner(UP + LEFT)
+        left1 = MathTex('< 180\\,{}^{\\circ} \\text{C}').to_corner(UP + LEFT)
+        left2 = MathTex('< 530\\,{}^{\\circ} \\text{C}').to_corner(UP + LEFT)
+        left3 = MathTex('> 530\\,{}^{\\circ} \\text{C}').to_corner(UP + LEFT)
+
+        right0 = Tex('Boric Acid').to_corner(UP + RIGHT)
+        right1 = Tex('Metaboric Acid').to_corner(UP + RIGHT)
+        right2 = Tex('Pyroboric Acid').to_corner(UP + RIGHT)
+        right3 = Tex('Boric Oxide').to_corner(UP + RIGHT)
+
         mol0 = Group(
-            readmol('BoricAcid').scale(0.8).shift(UP + (5 * LEFT)),
-            readmol('BoricAcid').scale(0.8).shift((1.333 * DOWN) + (1.333 * LEFT)),
-            readmol('BoricAcid').scale(0.8).shift((1.333 * UP) + (1.333 * RIGHT)),
-            readmol('BoricAcid').scale(0.8).shift(DOWN + (5 * RIGHT)),
+            readmol('BoricAcid').scale(0.85).shift(UP + (5 * LEFT)),
+            readmol('BoricAcid').scale(0.85).shift((1.333 * DOWN) + (1.333 * LEFT)),
+            readmol('BoricAcid').scale(0.85).shift((1.333 * UP) + (1.333 * RIGHT)),
+            readmol('BoricAcid').scale(0.85).shift(DOWN + (5 * RIGHT)),
         )
         mol1 = Group(
-            readmol('MetaboricAcid').scale(0.8).shift(UP + (5 * LEFT)),
-            readmol('MetaboricAcid').scale(0.8).shift((1.333 * DOWN) + (1.333 * LEFT)),
-            readmol('MetaboricAcid').scale(0.8).shift((1.333 * UP) + (1.333 * RIGHT)),
-            readmol('MetaboricAcid').scale(0.8).shift(DOWN + (5 * RIGHT)),
+            readmol('MetaboricAcid').scale(1.2).shift(UP + (5 * LEFT)),
+            readmol('MetaboricAcid').scale(1.2).shift((1.333 * DOWN) + (1.333 * LEFT)),
+            readmol('MetaboricAcid').scale(1.2).shift((1.333 * UP) + (1.333 * RIGHT)),
+            readmol('MetaboricAcid').scale(1.2).shift(DOWN + (5 * RIGHT)),
         )
         mol2 = Group(
-            readmol('TetraboricAcid'),
+            readmol('TetraboricAcid').scale(1.2),
         )
         mol3 = Group(
-            readmol('BoricOxide').scale(0.8).shift((1.333 * DOWN) + (1.333 * LEFT)),
-            readmol('BoricOxide').scale(0.8).shift((1.333 * UP) + (1.333 * RIGHT)),
+            readmol('BoricOxide').scale(1.2).shift((1.333 * DOWN) + (1.333 * LEFT)),
+            readmol('BoricOxide').scale(1.2).shift((1.333 * UP) + (1.333 * RIGHT)),
         )
-        self.add(mol0)
+        self.play(FadeIn(mol0), Write(left0), Write(right0))
         self.wait(1)
-        self.play(LaggedStart(
+        self.play(Transform(left0, left1), Transform(right0, right1), LaggedStart(
             (
                 AnimationGroup(FadeOut(mol0[i]), FadeIn(mol1[i]))
                 for i in range(4)
@@ -40,7 +48,7 @@ class Top(Scene):
             lag_ratio= 0.15,
         ))
         self.wait(1)
-        self.play(LaggedStart(
+        self.play(Transform(left0, left2), Transform(right0, right2), LaggedStart(
             [
                 LaggedStart(
                     [
@@ -55,4 +63,76 @@ class Top(Scene):
             ],
             lag_ratio= 0.1,
         ))
+        self.wait(1)
+        self.play(Transform(left0, left3), Transform(right0, right3), LaggedStart(
+            [
+                FadeOut(mol2[0]),
+                FadeIn(mol3[0]),
+                FadeIn(mol3[1]),
+            ],
+            lag_ratio= 0.15
+        ))
+        self.wait(1)
+        self.play(LaggedStart(
+            [
+                LaggedStart(
+                    [
+                        mol3[0].get_atoms_vgroup_from_index([3]).animate.set_opacity(0),
+                        mol3[0].get_bonds_vgroup_from_index([1], 0)[1].animate.set_opacity(0),
+                        mol3[0].get_bonds_vgroup_from_index([1], 0)[0].animate.set_opacity(0),
+                        mol3[0].get_atoms_vgroup_from_index([2]).animate.set_opacity(0),
+                        mol3[0].get_bonds_vgroup_from_index([4], 0)[0].animate.set_opacity(0),
+                        mol3[0].get_bonds_vgroup_from_index([4], 0)[1].animate.set_opacity(0),
+                        mol3[0].get_atoms_vgroup_from_index([5]).animate.set_opacity(0),
+                    ],
+                    lag_ratio= 0.1,
+                ),
+                LaggedStart(
+                    [
+                        mol3[1].get_atoms_vgroup_from_index([3]).animate.set_opacity(0),
+                        mol3[1].get_bonds_vgroup_from_index([1], 0)[1].animate.set_opacity(0),
+                        mol3[1].get_bonds_vgroup_from_index([1], 0)[0].animate.set_opacity(0),
+                        mol3[1].get_atoms_vgroup_from_index([2]).animate.set_opacity(0),
+                        mol3[1].get_bonds_vgroup_from_index([4], 0)[0].animate.set_opacity(0),
+                        mol3[1].get_bonds_vgroup_from_index([4], 0)[1].animate.set_opacity(0),
+                        mol3[1].get_atoms_vgroup_from_index([5]).animate.set_opacity(0),
+                    ],
+                    lag_ratio= 0.1,
+                ),
+            ],
+            lag_ratio= 0.08,
+        ))
+        self.wait(1)
+        proton = lambda: \
+            Dot(radius= 0.7, color= ManimColor('#FF0000'))
+        neutron = lambda: \
+            Dot(radius= 0.7, color= ManimColor('#0000FF'))
+        sc = 3.5
+        nuc_10b = VGroup(
+            proton().shift(sc*((+0.1 * UP) + (-0.2 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (-0.1 * LEFT))),
+            neutron().shift(sc*((+0.3 * UP) + (-0.1 * LEFT))),
+            proton().shift(sc*((-0.2 * UP) + (+0.1 * LEFT))),
+            proton().shift(sc*((-0.1 * UP) + (-0.1 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (+0.2 * LEFT))),
+            neutron().shift(sc*((+0.3 * UP) + (+0.1 * LEFT))),
+            proton().shift(sc*((+0.2 * UP) + (+0.1 * LEFT))),
+            proton().shift(sc*((+0.1 * UP) + (-0.3 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (+0.2 * LEFT))),
+        )
+        nuc_11b = VGroup(
+            proton().shift(sc*((+0.1 * UP) + (-0.3 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (+0.2 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (+0.2 * LEFT))),
+            proton().shift(sc*((+0.2 * UP) + (+0.1 * LEFT))),
+            neutron().shift(sc*((+0.3 * UP) + (+0.1 * LEFT))),
+            neutron().shift(sc*((-0.1 * UP) + (-0.1 * LEFT))),
+            neutron().shift(sc*((+0.3 * UP) + (-0.1 * LEFT))),
+            proton().shift(sc*((-0.2 * UP) + (+0.1 * LEFT))),
+            proton().shift(sc*((+0.1 * UP) + (-0.2 * LEFT))),
+            neutron().shift(sc*((-0.2 * UP) + (-0.1 * LEFT))),
+            proton().shift(sc*((-0.1 * UP) + (-0.1 * LEFT))),
+        )
+        self.play(VGroup(*mol3, left0, right0).animate.scale(5.0, about_point= ORIGIN).set_opacity(0), run_time=0.75)
+        self.play(Create(nuc_10b), run_time=0.25)
         self.wait(1)
